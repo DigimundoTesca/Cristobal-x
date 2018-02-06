4edac36ed0359a32b6c5ce9f790acb3d6ca9a3f3
RichDi <drdr_2@hotmail.com>
2018-02-05 23:01:21 -0600

    Modified resend Message for Email

--- home/tasks.py
+++ home/tasks.py
@@ -7,23 +7,22 @@ from email.mime.text import MIMEText
 import smtplib
 
 
-@background(schedule=64800)
+@background(schedule=5)
 def time_on_emails(pk,user_speciality, html_content):
-    pregunta = Question.objects.get(pk=pk)
+    pregunta = Question.objects.get(pk=pk)    
 
     if pregunta.status == 'OP'and pregunta.pk==pk:
         sendmailform(user_speciality, html_content)
 
 
-def sendmailform(email_user, html_content):
-    print("Email De Task Enviado")
+def sendmailform(email_user, html_content):    
     if email_user:
         fromaddr = "albeitarfmvz@comunidad.unam.mx"
         toaddr = email_user
         msg = MIMEMultipart()
         msg['From'] = fromaddr
         msg['To'] = toaddr
-        msg['Subject'] = "Se ha generado una nueva pregunta en el grupo del cual usted es especialista."
+        msg['Subject'] = "Aun no se ah respondido la pregunta en el grupo del cual usted es especialista."
 
         body = html_content
 
